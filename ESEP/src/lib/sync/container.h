672c36dff416c9cc3687e533b247b632eb0f4f5a
davekessener <davekessener@gmail.com>
2018-04-08 18:00:45 +0200

    Added STL synchronization templates

---
+++ ESEP/src/lib/sync/container.h
@@ -0,0 +1,94 @@
+#ifndef ESEP_LIB_SYNC_CONTAINER_H
+#define ESEP_LIB_SYNC_CONTAINER_H
+
+#include <deque>
+#include <mutex>
+#include <condition_variable>
+
+namespace esep
+{
+	namespace sync
+	{
+		template<typename R, typename C, typename ... A>
+		struct MemberWrapper
+		{
+			typedef R (C::*cb_fn)(A...);
+
+			MemberWrapper(cb_fn f) : mF(f) { }
+
+			R operator()(C&& c, A&& ... a)
+			{
+				return (c.*mF)(std::forward<A>(a)...);
+			}
+
+		private:
+			cb_fn mF;
+		};
+
+		template
+		<
+			typename T,
+			typename C = std::deque<T>,
+			typename Access = MemberWrapper<T, C>,
+			typename Insert = MemberWrapper<void, C, const T&>,
+			typename Remove = MemberWrapper<void, C>
+		>
+		class Container
+		{
+			public:
+			typedef T value_type;
+			typedef C container_type;
+			typedef Access access_fn;
+			typedef Insert insert_fn;
+			typedef Remove remove_fn;
+			typedef std::unique_lock<std::mutex> lock_t;
+
+			public:
+				Container(
+					access_fn a = access_fn(&container_type::front),
+					insert_fn i = insert_fn(&container_type::push_back),
+					remove_fn r = remove_fn(&container_type::pop_front))
+				: mAccess(a), mInsert(i), mRemove(r) { }
+				void insert(const value_type&);
+				value_type remove( );
+			private:
+				container_type mContainer;
+				access_fn mAccess;
+				insert_fn mInsert;
+				remove_fn mRemove;
+				std::mutex mMutex;
+				std::condition_variable mCond;
+		};
+
+		template<typename T, typename C, typename A, typename I, typename R>
+		void Container<T, C, A, I, R>::insert(const value_type& o)
+		{
+			{
+				lock_t lock(mMutex);
+
+				mInsert(mContainer, o);
+			}
+
+			mCond.notify_all();
+		}
+
+		template<typename T, typename C, typename A, typename I, typename R>
+		T Container<T, C, A, I, R>::remove(void)
+		{
+			lock_t lock(mMutex);
+
+			while(mContainer.empty())
+			{
+				mCond.wait(lock, [this](void) { return !mContainer.empty(); });
+			}
+
+			value_type o(mAccess(mContainer));
+
+			mRemove(mContainer);
+
+			return o;
+		}
+	}
+}
+
+#endif
---
+++ ESEP/src/lib/sync/monitor.h
@@ -0,0 +1,37 @@
+#ifndef ESEP_LIB_SYNC_MONITOR_H
+#define ESEP_LIB_SYNC_MONITOR_H
+
+#include <mutex>
+
+#include "lib/function.h"
+
+namespace esep
+{
+	namespace sync
+	{
+		template<typename T>
+		class Monitor
+		{
+			typedef std::unique_lock<std::mutex> lock_t;
+
+			public:
+				Monitor(T&& o) : mObject(o) { }
+				template<typename F>
+					auto operator()(F&&) -> typename lib::FunctionTraits<F>::return_type;
+			private:
+				T mObject;
+				std::mutex mMutex;
+		};
+
+		template<typename T>
+		template<typename F>
+		auto Monitor<T>::operator()(F&& f) -> typename lib::FunctionTraits<F>::return_type
+		{
+			lock_t lock(mMutex);
+
+			return f(mObject);
+		}
+	}
+}
+
+#endif
