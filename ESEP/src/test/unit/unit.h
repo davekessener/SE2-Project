2caaa6e3fc474bb84776f9b5d3c92e9698b29b30
davekessener <davekessener@gmail.com>
2018-04-09 15:06:46 +0200

    Cleaned up unit tests

--- ESEP/src/test/unit/unit.h
+++ ESEP/src/test/unit/unit.h
@@ -6,43 +6,6 @@
 #include <vector>
 #include <stdexcept>
 
-#define UNIT_TEST(desc) \
-	mWriter.name = desc; \
-	mWriter = [this](void) -> void
-
-#define ASSERT_EQUALS(a,b) \
-	do { \
-		auto va = (a); \
-		auto vb = (b); \
-		if(va != vb) { \
-			MXT_THROW_E(std::logic_error, "Expected ", vb, " but got ", va, " instead!"); \
-		} } while(0)
-
-#define ASSERT_EACH_EQUALS(a,b) \
-	do { \
-		auto i1 = std::begin(a), i2 = std::end(a); \
-		auto j1 = std::begin(b), j2 = std::end(b); \
-		size_t l1 = 0, l2 = 0; \
-		for(auto i = i1 ; i != i2 ; ++i) ++l1; \
-		for(auto j = j1 ; j != j2 ; ++j) ++l2; \
-		if(l1 != l2) \
-			MXT_THROW_E(std::logic_error, "Container have different sizes: ", l1, " and ", l2, "!"); \
-		for(size_t i = 0 ; i1 != i2 ; ++i1, ++j1, ++i) { \
-			if(*i1 != *j1) \
-				MXT_THROW_E(std::logic_error, "Elements @", i, " differ: Expected ", *j1, " but got ", *i1, " instead!"); \
-		} } while(0)
-
-#define ASSERT_FAILURE(e) \
-	do { \
-		bool success = true; \
-		try { \
-			e; \
-			success = false; \
-		} catch(...) { } \
-		if(!success) \
-			MXT_THROW_E(std::logic_error, "Expected an exception but got none."); \
-	} while(0)
-
 namespace esep
 {
 	namespace test
