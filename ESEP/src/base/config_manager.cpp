79cf396c68731687ee716c1d7385b84f52cce459
Martin Brak <martin.brak@haw-hamburg.de>
2018-05-04 21:11:59 +0200

    Reimplemented configuration

--- ESEP/src/base/config_manager.cpp
+++ ESEP/src/base/config_manager.cpp
@@ -6,8 +6,6 @@
 #include "base/config_manager.h"
 #include "system.h"
 
-#define MXT_TIME_NOW std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())
-
 namespace esep { namespace base {
 
 ConfigManager::ConfigManager(communication::IRecipient *baseHandler, ConfigObject *config)
@@ -18,163 +16,223 @@ ConfigManager::ConfigManager(communication::IRecipient *baseHandler, ConfigObjec
 	, LIGHT_BARRIERS(System::instance().get<hal::LightBarriers>())
 	, LIGHTS(System::instance().get<hal::Lights>())
 	, MOTOR(System::instance().get<hal::Motor>())
-	, mStep(0)
+	, mState(State::STATE_0)
 	, mTimestamp(0)
+	, mTimestamp2(0)
 	, mHeightSensor(0)
+	, mStartToEnd(0)
+	, mStartToEndLong(0)
+	, mStartToEndSlow(0)
 	, mStartToHs(0)
-	, mStartToHsBackward(0)
-	, mStartToHsSlow(0)
 	, mHsToSwitch(0)
 	, mSwitchToEnd(0)
 	, mSlowFactor(0)
+	, mTimeTolerance(0)
 {
 
 }
 
 void ConfigManager::enter()
 {
+	mState = State::STATE_0;
 }
 
 void ConfigManager::leave()
 {
-	mStep = 0;
+	SWITCH.close();
+	MOTOR.right();
+	MOTOR.fast();
+	MOTOR.stop();
+	LIGHTS.turnOff(Light::YELLOW);
 }
 
 void ConfigManager::accept(std::shared_ptr<communication::Packet> packet)
 {
-	// clear (for now)
+	switch (mState)
+	{
+	case State::STATE_0 :
+		if(packet->message() == Message::RESUME)
+			{
+				LIGHTS.flash(Light::YELLOW, 500);
+				MOTOR.right();
+				MOTOR.start();
+
+				mState = State::STATE_1;
+			};
+	break;
+	case State::STATE_15 :
+		if(packet->message() == Message::CONFIG_DONE)
+		{
+			MOTOR.stop();
+			LIGHTS.turnOff(Light::YELLOW);
+		};
+	break;
+	default :
+		break;
+	}
 }
 
 void ConfigManager::handle(hal::HAL::Event event)
 {
-	typedef hal::HAL::Event Event;
-	typedef hal::LightBarriers::LightBarrier LightBarrier;
-	typedef communication::Packet::Location Location;
-	typedef communication::Packet::Message Message;
+	uint64_t actualTime;
 
-	std::chrono::milliseconds msTime;
-	uint32_t actualTime;
-
-	switch (mStep)
+	switch (mState)
 	{
-	case 0 :
+	case State::STATE_0 :
+		if(event == Event::LB_START && LIGHT_BARRIERS.isBroken(LightBarrier::LB_START))
+		{
+			LIGHTS.flash(Light::YELLOW, 500);
+			MOTOR.right();
+			MOTOR.start();
+
+			mState = State::STATE_1;
+		};
+	break;
+	case State::STATE_1 :
+		if(event == Event::LB_START && !LIGHT_BARRIERS.isBroken(LightBarrier::LB_START))
+		{
+			mTimestamp  = Timer::instance().elapsed();
+
+			mState = State::STATE_2;
+		};
+	break;
+	case State::STATE_2 :
+		if(event == Event::LB_SWITCH && LIGHT_BARRIERS.isBroken(LightBarrier::LB_SWITCH))
+		{
+			SWITCH.open();
+
+			mState = State::STATE_3;
+		};
+	break;
+	case State::STATE_3 :
+		if(event == Event::LB_END && LIGHT_BARRIERS.isBroken(LightBarrier::LB_END))
+		{
+			actualTime  = Timer::instance().elapsed();
+			mStartToEndLong = (uint32_t) actualTime - mTimestamp;
+			MOTOR.left();
+
+			mState = State::STATE_4;
+		};
+	break;
+	case State::STATE_4 :
+		if(event == Event::LB_SWITCH && !LIGHT_BARRIERS.isBroken(LightBarrier::LB_SWITCH))
+		{
+			SWITCH.close();
+
+			mState = State::STATE_5;
+		};
+	break;
+	case State::STATE_5 :
 		if(event == Event::LB_START && LIGHT_BARRIERS.isBroken(LightBarrier::LB_START))
 		{
 			MOTOR.right();
-			MOTOR.start();
-			//TODO measure HeightSensor
+			mTimestamp = Timer::instance().elapsed();
+			mTimestamp2 = Timer::instance().elapsed();
 
-			mStep = 1;
+			mState = State::STATE_6;
 		};
 	break;
-	case 1 :
-		if(event == hal::HAL::Event::LB_HEIGHTSENSOR && LIGHT_BARRIERS.isBroken(hal::LightBarriers::LightBarrier::LB_HEIGHTSENSOR))
+	case State::STATE_6 :
+		if(event == Event::LB_HEIGHTSENSOR && LIGHT_BARRIERS.isBroken(LightBarrier::LB_HEIGHTSENSOR))
 		{
-			MOTOR.left();
-			msTime = std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now().time_since_epoch());
-			mTimestamp  = msTime.count();
+			actualTime = Timer::instance().elapsed();
+			mStartToHs = (uint32_t) actualTime - mTimestamp2;
 
-			mStep = 2;
+			mState = State::STATE_7;
 		};
 	break;
-	case 2 :
-		if(event == hal::HAL::Event::LB_START && LIGHT_BARRIERS.isBroken(hal::LightBarriers::LightBarrier::LB_START))
+	case State::STATE_7 :
+		if(event == Event::LB_HEIGHTSENSOR && !LIGHT_BARRIERS.isBroken(LightBarrier::LB_HEIGHTSENSOR))
+		{
+			mTimestamp2 = Timer::instance().elapsed();
+
+			mState = State::STATE_8;
+		};
+	break;
+	case State::STATE_8 :
+		if(event == Event::LB_SWITCH && LIGHT_BARRIERS.isBroken(LightBarrier::LB_SWITCH))
+		{
+			actualTime = Timer::instance().elapsed();
+			mHsToSwitch = (uint32_t) actualTime - mTimestamp2;
+			SWITCH.open();
+
+			mState = State::STATE_9;
+		};
+	break;
+	case State::STATE_9 :
+		if(event == Event::LB_SWITCH && !LIGHT_BARRIERS.isBroken(LightBarrier::LB_SWITCH))
+		{
+			mTimestamp2 = Timer::instance().elapsed();
+
+			mState = State::STATE_10;
+		};
+	break;
+	case State::STATE_10 :
+		if(event == Event::LB_END && LIGHT_BARRIERS.isBroken(LightBarrier::LB_END))
+		{
+			actualTime = Timer::instance().elapsed();
+			mSwitchToEnd = (uint32_t) actualTime - mTimestamp2;
+			mStartToEnd = (uint32_t) actualTime - mTimestamp;
+			MOTOR.left();
+
+			mState = State::STATE_11;
+		};
+	break;
+	case State::STATE_11 :
+		if(event == Event::LB_SWITCH && !LIGHT_BARRIERS.isBroken(LightBarrier::LB_SWITCH))
+		{
+			SWITCH.close();
+
+			mState = State::STATE_12;
+		};
+	break;
+	case State::STATE_12 :
+		if(event == Event::LB_START && LIGHT_BARRIERS.isBroken(LightBarrier::LB_START))
 		{
 			MOTOR.slow();
 			MOTOR.right();
-			msTime = std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now().time_since_epoch());
-			actualTime  = msTime.count();
-			mStartToHsBackward = actualTime - mTimestamp;
-			mTimestamp = msTime.count();
+			mTimestamp = Timer::instance().elapsed();
 
-			mStep = 3;
+			mState = State::STATE_13;
 		};
 	break;
-	case 3 :
-		if(event == hal::HAL::Event::LB_HEIGHTSENSOR && LIGHT_BARRIERS.isBroken(hal::LightBarriers::LightBarrier::LB_HEIGHTSENSOR))
+	case State::STATE_13 :
+		if(event == Event::LB_SWITCH && LIGHT_BARRIERS.isBroken(LightBarrier::LB_SWITCH))
 		{
-			MOTOR.left();
-			MOTOR.fast();
-			msTime = std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now().time_since_epoch());
-			actualTime  = msTime.count();
-			mStartToHsSlow = actualTime - mTimestamp;
+			SWITCH.open();
+			//TODO measure HEIGHTSENSOR
 
-			mStep = 4;
+			mState = State::STATE_14;
 		};
 	break;
-	case 4 :
-		if(event == hal::HAL::Event::LB_START && LIGHT_BARRIERS.isBroken(hal::LightBarriers::LightBarrier::LB_START))
+	case State::STATE_14 :
+		if(event == Event::LB_END && LIGHT_BARRIERS.isBroken(LightBarrier::LB_END))
 		{
-			MOTOR.right();
-			msTime = std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now().time_since_epoch());
-			mTimestamp  = msTime.count();
-
-			mStep = 5;
-		};
-	break;
-	case 5 :
-		if(event == hal::HAL::Event::LB_HEIGHTSENSOR && LIGHT_BARRIERS.isBroken(hal::LightBarriers::LightBarrier::LB_HEIGHTSENSOR))
-		{
-			msTime = std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now().time_since_epoch());
-			actualTime  = msTime.count();
-			mStartToHs = actualTime - mTimestamp;
-
-			mStep = 6;
-		};
-	break;
-	case 6 :
-		if(event == hal::HAL::Event::LB_HEIGHTSENSOR && !LIGHT_BARRIERS.isBroken(hal::LightBarriers::LightBarrier::LB_HEIGHTSENSOR))
-		{
-			msTime = std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now().time_since_epoch());
-			mTimestamp  = msTime.count();
-
-			mStep = 7;
-		};
-	break;
-	case 7 :
-		if(event == hal::HAL::Event::LB_SWITCH && LIGHT_BARRIERS.isBroken(hal::LightBarriers::LightBarrier::LB_SWITCH))
-		{
-			msTime = std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now().time_since_epoch());
-			actualTime  = msTime.count();
-			SWITCH.open();  // CRITICAL: SWITCH COULD BE OPEN FOREVER
-			mHsToSwitch = actualTime - mTimestamp;
-
-			mStep = 8;
-		};
-	break;
-	case 8 :
-		if(event == hal::HAL::Event::LB_SWITCH && !LIGHT_BARRIERS.isBroken(hal::LightBarriers::LightBarrier::LB_SWITCH))
-		{
-			msTime = std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now().time_since_epoch());
-			mTimestamp  = msTime.count();
-
-			mStep = 9;
-		};
-	break;
-	case 9 :
-		if(event == hal::HAL::Event::LB_END && LIGHT_BARRIERS.isBroken(hal::LightBarriers::LightBarrier::LB_END))
-		{
-			msTime = std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now().time_since_epoch());
-			actualTime  = msTime.count();
-			MOTOR.stop();
+			actualTime = Timer::instance().elapsed();
 			SWITCH.close();
-			mSwitchToEnd = actualTime - mTimestamp;
-			mSlowFactor = mStartToHs / (float) mStartToHsSlow;
+			mStartToEndSlow = (uint32_t) actualTime - mTimestamp;
+			mSlowFactor = mStartToEnd / (float) mStartToEndSlow;
+			mTimeTolerance = 1 - ((mStartToEnd / (float) mStartToEndLong) + (mStartToEnd / (float) mStartToEndLong) * ConfigObject::TOLERANCE);
 
-			// Save to object
-//			mConfig->setHeightSensor(mHeightSensor);
-			mConfig->setHeightSensor(1);
-			mConfig->setStartToHs(mStartToHs);
-			mConfig->setHsToSwitch(mHsToSwitch);
-			mConfig->setSwitchToEnd(mSwitchToEnd);
-			mConfig->setSlowFactor(mSlowFactor);
-			mConfig->save();
+			// Save config
+			try {
+				mConfig->setHeightSensor(1); // TODO measure HEIGHTSENSOR
+				mConfig->setStartToHs(mStartToHs);
+				mConfig->setHsToSwitch(mHsToSwitch);
+				mConfig->setSwitchToEnd(mSwitchToEnd);
+				mConfig->setSlowFactor(mSlowFactor);
+				mConfig->setTimeTolerance(mTimeTolerance);
+				mConfig->save();
+			} catch (...) {
+				auto msg = std::make_shared<communication::Packet>(Location::BASE, Location::MASTER, Message::CONFIG_FAILED);
+				mBaseHandler->accept(msg);
+				return;
+			}
 
-			// send message
 			auto msg = std::make_shared<communication::Packet>(Location::BASE, Location::MASTER, Message::CONFIG_DONE);
 			mBaseHandler->accept(msg);
 
-			mStep = 10;
+			mState = State::STATE_15;
 		};
 	break;
 
--- ESEP/src/base/config_object.h
+++ ESEP/src/base/config_object.h
@@ -30,12 +30,14 @@ namespace esep
 				uint32_t hsToSwitch();		// Time between LB_HeightSensor and LB_Switch [ms]
 				uint32_t switchToEnd();		// Time between LB_Switch and LB_End [ms]
 				float slowFactor();			// Slow speed factor
+				float timeTolerance();		// Tolerance factor for time measuring [%]
 
 				void setHeightSensor(uint16_t);
 				void setStartToHs(uint32_t);
 				void setHsToSwitch(uint32_t);
 				void setSwitchToEnd(uint32_t);
 				void setSlowFactor(float);
+				void setTimeTolerance(float);
 
 			private:
 				const std::string mPath;
@@ -45,6 +47,7 @@ namespace esep
 				uint32_t mHsToSwitch;
 				uint32_t mSwitchToEnd;
 				float mSlowFactor;
+				float mTimeTolerance;
 		};
 
 }
