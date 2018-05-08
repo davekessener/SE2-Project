8de6808faf1b3ef0c30e2d5b9c2c1c46813116bb
Ahmed Hussein <ahmed.hussein@haw-hamburg.de>
2018-05-09 00:29:57 +0200

    Updated IdleManager

--- ESEP/src/base/idle_manager.cpp
+++ ESEP/src/base/idle_manager.cpp
@@ -11,6 +11,10 @@ namespace esep{ namespace base{
 		   void IdleManager::handle(hal::HAL::Event)
 		   {
 
+			   lib::Timer::instance().registerCallback([this](void){
+				   this->accept(communication::Packet(communication::Packet::Location::BASE, communication::Packet::Location::BASE, communication::Packet::Message::SELECT_CONFIG))
+			   },1000);
+
 		   }
 
 }}
