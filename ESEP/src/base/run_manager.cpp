08668fd6975000d9aef444e7b5b11f45e9701718
acg157 <dennis.sentler@haw-hamburg.de>
2018-05-28 15:39:34 +0200

    Doku

--- ESEP/src/base/run_manager.cpp
+++ ESEP/src/base/run_manager.cpp
@@ -118,6 +118,9 @@ void RunManager::acceptTimerEvent(run::TimerEvent e)
 	mLogic.process(e);
 }
 
+/**
+ * Sends a error message to master with source BASE and target MASTER
+ */
 void RunManager::sendErrorMessage(runMessage_t msg, data::Location::Type location)
 {
 	auto packet = std::make_shared<communication::Packet>(Location::BASE, Location::MASTER, msg);
