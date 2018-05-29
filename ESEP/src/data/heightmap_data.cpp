02c8cb4e5659c2b8cffebcd44c902d3470c436aa
ace991 <ace991@52-lab51.mailcluster.haw-hamburg.de>
2018-05-29 18:38:42 +0200

    Finished HeightMap

--- ESEP/src/data/heightmap_data.cpp
+++ ESEP/src/data/heightmap_data.cpp
@@ -6,11 +6,38 @@ namespace esep { namespace data {
 
 void HeightMap::doSerialize(lib::ByteStream& bs) const
 {
+	for(const auto& p : mHeightValue)
+	{
+		bs << p.first;
+		bs << p.second;
+	}
+}
+
+void HeightMap::addHeightValue(time_t time, height_t height)
+{
+	mHeightValue.emplace_back(std::make_pair(time, height));
 }
 
 Data_ptr HeightMap::deserialize(lib::ByteStream& bs)
 {
-	MXT_THROW_EX(DataManager::UnexpectedEOSException);
+	if(bs.empty())
+		{
+			MXT_THROW_EX(DataManager::UnexpectedEOSException);
+		}
+
+	std::unique_ptr<HeightMap> hm(new HeightMap);
+
+	while(!bs.empty())
+	{
+		uint32_t time;
+		bs >> time;
+		uint16_t height;
+		bs >> height;
+		hm->addHeightValue(time, height);
+	}
+
+	return Data_ptr(hm.release());
+
 }
 
 }}
