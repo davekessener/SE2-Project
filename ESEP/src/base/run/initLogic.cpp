cee2efac808d17638c5eea343139468aaf0f18f1
ace991 <ace991@52-lab51.mailcluster.haw-hamburg.de>
2018-05-29 17:22:44 +0200

    Deleted in LB timechecking

--- ESEP/src/base/run/initLogic.cpp
+++ ESEP/src/base/run/initLogic.cpp
@@ -35,10 +35,8 @@ void RunManager::initLogic()
 			{},
 			[this](void)
 			{
-				auto t = data::Location::Type::HAND_OVER;
-				auto l = new data::Location(t);
-				data::Data_ptr p(l);
-				this->sendMessage(Location::MASTER, runMessage_t::ITEM_DISAPPEARED, std::move(p));
+				this->mTimeCtrl.deleteTimer(State::STATE_1);
+				this->sendMessageWithData(Location::MASTER, runMessage_t::ITEM_DISAPPEARED, MXT_SHARE(data::Location::Type::LB_START));
 			});
 	//LB_START_1
 	mLogic.transition(run::HalEvent::LB_START,
@@ -47,7 +45,7 @@ void RunManager::initLogic()
 			[this](void)
 			{
 				this->mTimeCtrl.deleteTimer(State::STATE_1);
-				this->mTimeCtrl.setTimer(State::STATE_2, TimerEvent::START_1, MXT_TIME_IN_LB);
+				//this->mTimeCtrl.setTimer(State::STATE_2, TimerEvent::START_1, MXT_TIME_IN_LB);
 			});
 	//LB_START_2
 	mLogic.transition(run::HalEvent::LB_START,
@@ -55,7 +53,7 @@ void RunManager::initLogic()
 			{{MXT_CAST(State::STATE_2), 1}},
 			[this](void)
 			{
-				this->mTimeCtrl.setTimer(State::STATE_2, TimerEvent::START_1, MXT_TIME_IN_LB);
+				//this->mTimeCtrl.setTimer(State::STATE_2, TimerEvent::START_1, MXT_TIME_IN_LB);
 			});
 	//TIMER_START_1
 	mLogic.transition(TimerEvent::START_1,
@@ -63,7 +61,7 @@ void RunManager::initLogic()
 			{},
 			[this](void)
 			{
-				this->sendMessage(Location::MASTER, runMessage_t::ITEM_DISAPPEARED, MXT_SHARE(data::Location::Type::LB_START));
+				//this->sendMessage(Location::MASTER, runMessage_t::ITEM_DISAPPEARED, MXT_SHARE(data::Location::Type::LB_START));
 			});
 	//!LB_START
 	mLogic.transition(run::HalEvent::I_LB_START,
@@ -71,9 +69,9 @@ void RunManager::initLogic()
 			{{MXT_CAST(State::STATE_3), 1}},
 			[this](void)
 			{
-				this->mTimeCtrl.deleteTimer(State::STATE_2);
+				//this->mTimeCtrl.deleteTimer(State::STATE_2);
 				auto minTimeStartToHs = computeMinTime(MXT_CONFIG->startToHs());
-				minTimeStartToHs =- minTimeStartToHs * MXT_CONFIG->timeTolerance();
+				minTimeStartToHs *= (1 - MXT_CONFIG->timeTolerance());
 				this->mTimeCtrl.setTimer(State::STATE_3, TimerEvent::ITEM_READY_HS, minTimeStartToHs);
 			});
 	//ITEM_READY_HS
@@ -82,11 +80,13 @@ void RunManager::initLogic()
 			{{MXT_CAST(State::STATE_4), 1}},
 			[this](void)
 			{
+				this->mTimeCtrl.deleteTimer(State::STATE_3);
+
 				auto minTimeStartToHs = computeMinTime(MXT_CONFIG->startToHs());
-				minTimeStartToHs =- minTimeStartToHs * MXT_CONFIG->timeTolerance();
+				minTimeStartToHs *= (1 - MXT_CONFIG->timeTolerance());
 
 				auto maxTimeStartToHs = computeMaxTime(MXT_CONFIG->startToHs());
-				maxTimeStartToHs =+ maxTimeStartToHs * MXT_CONFIG->timeTolerance();
+				maxTimeStartToHs *= (1 + MXT_CONFIG->timeTolerance());
 				this->mTimeCtrl.setTimer(State::STATE_4, TimerEvent::START_2, maxTimeStartToHs-minTimeStartToHs);
 			});
 	//TIMER_START_2
@@ -95,7 +95,8 @@ void RunManager::initLogic()
 			{},
 			[this](void)
 			{
-				this->sendMessage(Location::MASTER, runMessage_t::ITEM_DISAPPEARED, MXT_SHARE(data::Location::Type::LB_START));
+				this->mTimeCtrl.deleteTimer(State::STATE_4);
+				this->sendMessageWithData(Location::MASTER, runMessage_t::ITEM_DISAPPEARED, MXT_SHARE(data::Location::Type::LB_HEIGHTSENSOR));
 			});
 	//LB_HS
 	mLogic.transition(run::HalEvent::LB_HS,
@@ -104,7 +105,7 @@ void RunManager::initLogic()
 			[this](void)
 			{
 				this->mTimeCtrl.deleteTimer(State::STATE_4);
-				this->mTimeCtrl.setTimer(State::STATE_5, TimerEvent::HS_1, MXT_TIME_IN_LB);
+				//this->mTimeCtrl.setTimer(State::STATE_5, TimerEvent::HS_1, MXT_TIME_IN_LB);
 			});
 	//LB_HS_E
 	mLogic.transition(run::HalEvent::LB_HS,
@@ -112,7 +113,7 @@ void RunManager::initLogic()
 			{},
 			[this](void)
 			{
-				this->sendMessage(Location::MASTER, runMessage_t::ITEM_APPEARED, MXT_SHARE(data::Location::Type::LB_HEIGHTSENSOR));
+				this->sendMessageWithData(Location::MASTER, runMessage_t::ITEM_APPEARED, MXT_SHARE(data::Location::Type::LB_HEIGHTSENSOR));
 			});
 
 	//--------- Hoehenmessung bis zum Switch
@@ -122,7 +123,7 @@ void RunManager::initLogic()
 			{},
 			[this](void)
 			{
-				this->sendMessage(Location::MASTER, runMessage_t::ITEM_DISAPPEARED, MXT_SHARE(data::Location::Type::LB_HEIGHTSENSOR));
+				//this->sendMessage(Location::MASTER, runMessage_t::ITEM_DISAPPEARED, MXT_SHARE(data::Location::Type::LB_HEIGHTSENSOR));
 			});
 
 	//!LB_HS
@@ -131,7 +132,7 @@ void RunManager::initLogic()
 			{{MXT_CAST(State::STATE_6), 1}},
 			[this](void)
 			{
-				this->mTimeCtrl.deleteTimer(State::STATE_5);
+				//this->mTimeCtrl.deleteTimer(State::STATE_5);
 				this->mTimeCtrl.setTimer(State::STATE_6, TimerEvent::ITEM_READY_SWITCH, computeMinTime(MXT_CONFIG->hsToSwitch()));
 			});
 
@@ -141,6 +142,7 @@ void RunManager::initLogic()
 			{{MXT_CAST(State::STATE_7), 1}},
 			[this](void)
 			{
+				this->mTimeCtrl.deleteTimer(State::STATE_6);
 				auto maxTimeDiff = computeMaxTime(MXT_CONFIG->hsToSwitch()) - computeMinTime(MXT_CONFIG->hsToSwitch());
 				this->mTimeCtrl.setTimer(State::STATE_7, TimerEvent::HS_2, maxTimeDiff);
 			});
@@ -151,7 +153,8 @@ void RunManager::initLogic()
 			{},
 			[this](void)
 			{
-				this->sendMessage(Location::MASTER, runMessage_t::ITEM_DISAPPEARED, MXT_SHARE(data::Location::Type::LB_HEIGHTSENSOR));
+				this->mTimeCtrl.deleteTimer(State::STATE_7);
+				this->sendMessageWithData(Location::MASTER, runMessage_t::ITEM_DISAPPEARED, MXT_SHARE(data::Location::Type::LB_SWITCH));
 			});
 	//LB_SWITCH
 	mLogic.transition(run::HalEvent::LB_SWITCH,
@@ -159,7 +162,8 @@ void RunManager::initLogic()
 			{{MXT_CAST(State::STATE_8), 1}},
 			[this](void)
 			{
-				this->mTimeCtrl.setTimer(State::STATE_8, TimerEvent::SWITCH_1, MXT_TIME_IN_LB);
+				this->mTimeCtrl.deleteTimer(State::STATE_7);
+				//this->mTimeCtrl.setTimer(State::STATE_8, TimerEvent::SWITCH_1, MXT_TIME_IN_LB);
 			});
 	//LB_SWITCH_E
 	mLogic.transition(run::HalEvent::LB_SWITCH,
@@ -167,7 +171,7 @@ void RunManager::initLogic()
 			{},
 			[this](void)
 			{
-				this->sendMessage(Location::MASTER, runMessage_t::ITEM_APPEARED, MXT_SHARE(data::Location::Type::LB_SWITCH));
+				this->sendMessageWithData(Location::MASTER, runMessage_t::ITEM_APPEARED, MXT_SHARE(data::Location::Type::LB_SWITCH));
 			});
 
 	//--------- Switch und Rampe
@@ -177,8 +181,8 @@ void RunManager::initLogic()
 			{{MXT_CAST(State::STATE_11), 1}},
 			[this](void)
 			{
-				this->mTimeCtrl.deleteTimer(State::STATE_8);
-				this->mTimeCtrl.setTimer(State::STATE_11, TimerEvent::SWITCH_2, MXT_TIME_IN_LB);
+				//this->mTimeCtrl.deleteTimer(State::STATE_8);
+				//this->mTimeCtrl.setTimer(State::STATE_11, TimerEvent::SWITCH_2, MXT_TIME_IN_LB);
 			});
 	//TIMER_SWITCH_1
 	mLogic.transition(TimerEvent::SWITCH_1,
@@ -186,7 +190,7 @@ void RunManager::initLogic()
 			{},
 			[this](void)
 			{
-				this->sendMessage(Location::MASTER, runMessage_t::ITEM_DISAPPEARED, MXT_SHARE(data::Location::Type::LB_SWITCH));
+				//this->sendMessage(Location::MASTER, runMessage_t::ITEM_DISAPPEARED, MXT_SHARE(data::Location::Type::LB_SWITCH));
 			});
 	//!LB_SWITCH
 	mLogic.transition(run::HalEvent::I_LB_SWITCH,
@@ -194,7 +198,7 @@ void RunManager::initLogic()
 			{{MXT_CAST(State::STATE_9), 1}},
 			[this](void)
 			{
-				this->mTimeCtrl.deleteTimer(State::STATE_8);
+				//this->mTimeCtrl.deleteTimer(State::STATE_8);
 			});
 	//LB_RAMP
 	mLogic.transition(run::HalEvent::LB_RAMP,
@@ -210,7 +214,7 @@ void RunManager::initLogic()
 			{},
 			[this](void)
 			{
-				this->sendMessage(Location::MASTER, runMessage_t::ITEM_APPEARED, MXT_SHARE(data::Location::Type::LB_RAMP));
+				this->sendMessageWithData(Location::MASTER, runMessage_t::ITEM_APPEARED, MXT_SHARE(data::Location::Type::LB_RAMP));
 			});
 	//TIMER_RAMP
 	mLogic.transition(TimerEvent::RAMP,
@@ -218,7 +222,8 @@ void RunManager::initLogic()
 			{},
 			[this](void)
 			{
-				this->sendMessage(Location::MASTER, runMessage_t::RAMP_FULL, MXT_SHARE(data::Location::Type::LB_RAMP));
+				this->mTimeCtrl.deleteTimer(State::STATE_10);
+				this->sendMasterMessage(runMessage_t::RAMP_FULL);
 			});
 	//TIMER_RAMP
 	mLogic.transition(run::HalEvent::I_LB_RAMP,
@@ -236,15 +241,15 @@ void RunManager::initLogic()
 			{},
 			[this](void)
 			{
-				this->sendMessage(Location::MASTER, runMessage_t::ITEM_DISAPPEARED, MXT_SHARE(data::Location::Type::LB_SWITCH));
+				//this->sendMessage(Location::MASTER, runMessage_t::ITEM_DISAPPEARED, MXT_SHARE(data::Location::Type::LB_SWITCH));
 			});
 	//!LB_SWITCH_2
-	mLogic.transition(TimerEvent::SWITCH_2,
+	mLogic.transition(run::HalEvent::I_LB_SWITCH,
 			{{MXT_CAST(State::STATE_11), 1}},
 			{{MXT_CAST(State::STATE_12), 1}},
 			[this](void)
 			{
-				this->mTimeCtrl.deleteTimer(State::STATE_11);
+				//this->mTimeCtrl.deleteTimer(State::STATE_11);
 				this->mTimeCtrl.setTimer(State::STATE_12, TimerEvent::ITEM_READY_END, computeMinTime(MXT_CONFIG->switchToEnd()));
 			});
 	//ITEM_READY_END
@@ -253,6 +258,7 @@ void RunManager::initLogic()
 			{{MXT_CAST(State::STATE_13), 1}},
 			[this](void)
 			{
+				this->mTimeCtrl.deleteTimer(State::STATE_12);
 				auto maxTimeDiff = computeMaxTime(MXT_CONFIG->switchToEnd()) - computeMinTime(MXT_CONFIG->switchToEnd());
 				this->mTimeCtrl.setTimer(State::STATE_13, TimerEvent::SWITCH_3, maxTimeDiff);
 			});
@@ -262,7 +268,8 @@ void RunManager::initLogic()
 			{},
 			[this](void)
 			{
-				this->sendMessage(Location::MASTER, runMessage_t::ITEM_DISAPPEARED, MXT_SHARE(data::Location::Type::LB_SWITCH));
+				this->mTimeCtrl.deleteTimer(State::STATE_13);
+				this->sendMessageWithData(Location::MASTER, runMessage_t::ITEM_DISAPPEARED, MXT_SHARE(data::Location::Type::LB_END));
 			});
 	//LB_END
 	mLogic.transition(run::HalEvent::LB_END,
@@ -285,12 +292,12 @@ void RunManager::initLogic()
 
 uint32_t RunManager::computeMinTime(uint32_t time)
 {
-	return time-(time*mConfigData->TOLERANCE);
+	return time * (1 - mConfigData->TOLERANCE);
 }
 
 uint32_t RunManager::computeMaxTime(uint32_t time)
 {
-	return time+(time*mConfigData->TOLERANCE);
+	return time * (1 + mConfigData->TOLERANCE);
 }
 
 }}
