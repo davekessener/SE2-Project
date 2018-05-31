8f19d6210befcbab3f13312b4e23148ff83ac74c
ace991 <ace991@52-lab04.mailcluster.haw-hamburg.de>
2018-05-31 15:54:11 +0200

    Added run_manager basic

---
+++ ESEP/src/test/ut/run_manager_logic.cpp
@@ -0,0 +1,19 @@
+#include "test/ut/run_manager_logic.h"
+
+#include "test/unit/assertions.h"
+
+namespace esep { namespace test { namespace unit {
+
+RunManagerLogic::RunManagerLogic()
+	//:TestSuite("Run Manager")
+
+{
+
+}
+
+RunManagerLogic::~RunManagerLogic()
+{
+
+}
+
+}}}
---
+++ ESEP/src/test/ut/run_manager_logic.h
@@ -0,0 +1,34 @@
+
+#ifndef SRC_TEST_UT_RUN_MANAGER_LOGIC_H
+#define SRC_TEST_UT_RUN_MANAGER_LOGIC_H
+
+#include "test/unit/unit.h"
+#include "base/run_manager.h"
+
+namespace esep
+{
+	namespace test
+	{
+		namespace unit
+		{
+
+			class RunManagerLogic // : public TestSuite
+			{
+				public:
+				   RunManagerLogic();
+				   virtual ~RunManagerLogic();
+
+				protected:
+//				   void setup( ) override;
+//				   void teardown( ) override;
+//				   void define( ) override;
+
+				private:
+
+			};
+
+		}
+	}
+}
+
+#endif /* SRC_TEST_UT_RUN_MANAGER_LOGIC_H */
